d0ffe8b2ed75c5588fa4632220fe478331092ec6
James Cussens <james.cussens@york.ac.uk>
2020-06-08 12:19:57 +0100

    improved test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,20 @@
 from pygobnilp.gobnilp import Gobnilp
 m = Gobnilp()
 
+epsilon = 0.0001
+def optimal_score_is(m,score):
+    return abs(m.learned_scores[0] - score) < epsilon
+
 def test_bge():
     m.learn('data/gaussian.dat',data_type='continuous',score='BGe',plot=False,palim=None)
-    assert abs(m.learned_scores[0] - -53258.94161814058) < 0.0001
+    assert optimal_score_is(m, -53258.94161814058)
 
 def test_bic():
     m.learn('data/gaussian.dat',data_type='continuous',score='GaussianBIC',plot=False,palim=None)
-    assert abs(m.learned_scores[0] - -53221.34568733569) < 0.0001
+    assert optimal_score_is(m, -53221.34568733569)
     m.learn('data/gaussian.dat',data_type='continuous',score='GaussianBIC',plot=False,palim=None,sdresidparam=False)
-    assert abs(m.learned_scores[0] - -53191.53551116573) < 0.0001
+    assert optimal_score_is(m, -53191.53551116573)
 
 def test_bdeu():
     m.learn('data/discrete.dat',plot=False,palim=None)
-    assert abs(m.learned_scores[0] - -24028.0947783535) < 0.0001
+    assert optimal_score_is(m, -24028.0947783535)
